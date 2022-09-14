c7a6e57a52994d91c377ae100cd378b36b8a8ab1
olawale <jao.olaniyi@gmail.com>
2022-09-14 08:04:32 +0100

    added the main css file

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -6,9 +6,9 @@ const Main = () => {
     <div>
         <main>
       {/* HEROSECTION  */}
-      <section class="heroSection">
-        <div class="heroSubOne">
-          <div class="heroBackgrond">
+      <section className="heroSection">
+        <div className="heroSubOne">
+          <div className="heroBackgrond">
             <img src="./Assets/HeroBg.svg" alt="HeroBackGround" />
           </div>
           <h3>Best Destinations around the world</h3>
@@ -19,46 +19,46 @@ const Main = () => {
             west hard for the.
           </p>
           <aside>
-            <button class="btn1">Find out more</button>
-            <div class="iframe" ><a href="https://youtu.be/cTT7E6M-1Js"><img src="./Assets/PlayButton.svg" alt="PlayButton"></a>
+            <button className="btn1">Find out more</button>
+            <div className="iframe" ><a href="https://youtu.be/cTT7E6M-1Js"><img src="./Assets/PlayButton.svg" alt="PlayButton"/></a>
             </div>
             Play Demo
           </aside>
         </div>
-        <div class="heroImage">
+        <div className="heroImage">
           <img src="./Assets/TravellerGirlPlane.svg" alt="TravellerGirl" />
         </div>
       </section>
       {/* SERVICES SECTION  */}
-      <section class="services">
-        <div class="servicesText">
+      <section className="services">
+        <div className="servicesText">
           <img src="./Assets/tinyStars.svg" alt="tinyStars" />
           <h6>CATEGORY</h6>
           <h3>We Offer Best Services</h3>
         </div>
-        <div class="servicesLists">
-          <div class="servicesRendered">
+        <div className="servicesLists">
+          <div className="servicesRendered">
             <img src="./Assets/sateliteDish.svg" alt="sateliteDish" />
             <h6>Calculated Weather</h6>
             <p>Built Wicket longer admire do barton vanity itself do in it.</p>
-            <div class="box"></div>
+            <div className="box"></div>
           </div>
-          <div class="servicesRendered">
+          <div className="servicesRendered">
             <img src="./Assets/bestFlight.svg" alt="bestFlight" />
             <h6>Best Flights</h6>
             <p>Engrossed listening. Park gate sell they west hard for the.</p>
-            <div class="box"></div>
+            <div className="box"></div>
           </div>
-          <div class="servicesRendered">
+          <div className="servicesRendered">
             <img src="./Assets/LocalEvents.svg" alt="LocalEvents" />
             <h6>Local Events</h6>
             <p>
               Barton vanity itself do in it. Preferd to men it engrossed
               listening.
             </p>
-            <div class="box"></div>
+            <div className="box"></div>
           </div>
-          <div class="servicesRendered">
+          <div className="servicesRendered">
             <img
               src="./Assets/customizationSettings.svg"
               alt="customizationSettings"
@@ -67,61 +67,61 @@ const Main = () => {
             <p>
               We deliver outsourced aviation services for military customers
             </p>
-            <div class="box"></div>
+            <div className="box"></div>
           </div>
         </div>
       </section>
        {/* DESTINATION SECTION  */}
-      <section id="destination" class="destination">
-        <div class="destinationText">
+      <section id="destination" className="destination">
+        <div className="destinationText">
           <h6>Top Selling</h6>
           <h3>Top Destinations</h3>
-          <!-- <div class="sketches"> 
+           {/* <div className="sketches"> 
           <img src="./Assets/roughSketch.svg" alt="roughSketch">
-          </div> -->
+          </div>  */}
         </div>
-        <div class="destinationCities">
-          <div class="cities">
-            <div class="cityImg">
+        <div className="destinationCities">
+          <div className="cities">
+            <div className="cityImg">
               <img src="./Assets/romeDistination.png" alt="destinationRome" />
             </div>
-            <div class="cityTrip">
-              <span class="city">
+            <div className="cityTrip">
+              <span className="city">
                 <p>Rome, Italy</p>
                 <p>$5,42k</p>
               </span>
-              <span class="trip">
+              <span className="trip">
                 <img src="./Assets/sendDestination.svg" alt="sendDestination" />
                 <p>10 Days Trip</p>
               </span>
             </div>
           </div>
-          <div class="cities">
-            <div class="cityImg">
+          <div className="cities">
+            <div className="cityImg">
             <img src="./Assets/londonDistination.png" alt="destinationUK" />
           </div>
-          <div class="cityTrip">
+          <div className="cityTrip">
 
-            <div class="city">
+            <div className="city">
               <p>London, UK</p>
               <p>$4.2k</p>
             </div>
-            <div class="trip">
+            <div className="trip">
               <img src="./Assets/sendDestination.svg" alt="sendDestination" />
               <p>12 Days Trip</p>
             </div>
           </div>
           </div>
-          <div class="cities">
-            <div class="cityImg">
+          <div className="cities">
+            <div className="cityImg">
             <img src="./Assets/europeDistination1.png" alt="destinationEurope" />
           </div>
-          <div class="cityTrip">
-            <span class="city">
+          <div className="cityTrip">
+            <span className="city">
               <p>Full Europe</p>
               <p>$5,42k</p>
             </span>
-            <span class="trip">
+            <span className="trip">
               <img src="./Assets/sendDestination.svg" alt="sendDestination" />
               <p>28 Days Trip</p>
             </span>
@@ -129,11 +129,11 @@ const Main = () => {
           </div>
         </div>
       </section>
-      <section id="stepToBook" class="stepsToBook">
-        <div class="stepText">
+      <section id="stepToBook" className="stepsToBook">
+        <div className="stepText">
           <h5>Easy and Fast</h5>
           <h2>Book your next trip in 3 easy steps</h2>
-          <span class="steps">
+          <span className="steps">
             <img src="./Assets/chooseDestination.svg" alt="chooseDestination" />
             <span>
               <h6>Choose Destination</h6>
@@ -143,7 +143,7 @@ const Main = () => {
               </p>
             </span>
           </span>
-          <span class="steps">
+          <span className="steps">
             <img src="./Assets/makePayment.svg" alt="makePayment" />
             <span>
               <h6>Make Payment</h6>
@@ -153,7 +153,7 @@ const Main = () => {
               </p>
             </span>
           </span>
-          <span class="steps">
+          <span className="steps">
             <img src="./Assets/arrivalTime.svg" alt="" />
             <span>
               <h6>Reach Airport on Selected Date</h6>
@@ -164,8 +164,8 @@ const Main = () => {
             </span>
           </span>
         </div>
-        <div class="blurWrapper">
-          <div class="tripPackage">
+        <div className="blurWrapper">
+          <div className="tripPackage">
             <img src="./Assets/tripToGreece.svg" alt="tripToGreece" />
             <h6>Trip To Greece</h6>
             <p>14-29 June| by Robbin joseph</p>
@@ -174,106 +174,106 @@ const Main = () => {
               <img src="./Assets/mapIcon.svg" alt="mapIcon" />
               <img src="./Assets/send.svg" alt="send" />
             </span>
-            <span class="peopleLikes">
-              <span class="peopleGoing">
+            <span className="peopleLikes">
+              <span className="peopleGoing">
                 <img src="./Assets/peopleGoing.svg" alt="peopleGoing" />
                 <p>24 people going</p>
               </span>
               <img src="./Assets/likes.svg" alt="likes" />
             </span>
-            <div class="tripToRome">
-              <div class="outGoing">
+            <div className="tripToRome">
+              <div className="outGoing">
                 <img src="./Assets/tripToRome.svg" alt="tripToRome" />
-                <div class="going">
+                <div className="going">
                   <h6>Ongoing</h6>
                   <p>Trip to rome</p>
                 </div>
               </div>
               <h6>40% completed</h6>
-              <div class="container">
-                <div class="skills progress"></div>
+              <div className="container">
+                <div className="skills progress"></div>
               </div>
             </div>
-            <div class="ecilpse"></div>
+            <div className="ecilpse"></div>
             </div>
         </div>
        </section>
         {/* TESTIMONIALS  */}
-     <section class="testimonial">
-        <div class="testimonies">
+     <section className="testimonial">
+        <div className="testimonies">
           <h5>Testimonials</h5>
           <h2>What people say
             about Us.</h2>
-            <!-- ONCLICK JAVASCRIPT FUNCTION FOR THE CAROUSEL -->
-            <div class="carouselDot" onclick="currentSlide(1)"> </div>
-            <div class= "carouselDot" onclick="currentSlide(2)"></div>
-            <div class="carouselDot" onclick="currentSlide(3)"></div>
+             {/* ONCLICK JAVASCRIPT FUNCTION FOR THE CAROUSEL  */}
+            <div className="carouselDot" onclick="currentSlide(1)"> </div>
+            <div className= "carouselDot" onclick="currentSlide(2)"></div>
+            <div className="carouselDot" onclick="currentSlide(3)"></div>
         </div>
         <div>
-          <div class="carouselTestimony carouselTestimony__visible">
-            <img src="./Assets/ImageTestimonial.svg" alt="ImageTestimonial">
+          <div className="carouselTestimony carouselTestimony__visible">
+            <img src="./Assets/ImageTestimonial.svg" alt="ImageTestimonial"/>
             <p>“On the Windows talking painted pasture yet its express parties use. Sure last upon he same as knew next. Of believed or diverted no.”</p>
             <h6>Mike taylor</h6> 
             <h6>Lahore, Pakistan</h6>
           </div>
-          <div class="carouselTestimony carouselTestimony__hidden">
-            <img src="./Assets/ImageTestimonial.svg" alt="ImageTestimonial">
+          <div className="carouselTestimony carouselTestimony__hidden">
+            <img src="./Assets/ImageTestimonial.svg" alt="ImageTestimonial"/>
             <p>“Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet deserunt tempore ducimus unde aperiam ullam maiores inventore nesciunt nam consequuntur mollitia nostrum doloribus ad soluta, porro necessitatibus perferendis architecto dicta.”</p>
             <h6>Mike taylor</h6>
             <h6>Lahore, Pakistan</h6>
           </div>
-          <div class="carouselTestimony carouselTestimony__hidden">
-            <img src="./Assets/ImageTestimonial.svg" alt="ImageTestimonial">
+          <div className="carouselTestimony carouselTestimony__hidden">
+            <img src="./Assets/ImageTestimonial.svg" alt="ImageTestimonial"/>
             <p>“Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint ab natus laudantium consequatur ipsam ex..”</p>
             <h6>Mike taylor</h6>
             <h6>Lahore, Pakistan</h6>
           </div>
-          <div class="arrow">
-            <img class="arrowUp" onclick="plusSlides(-1)" src="./Assets/arrowUp.svg" alt="arrowUp">
-            <img class="
-            arrowDown" onclick="plusSlides(1)" src="./Assets/arrowDown.svg" alt="arrowDown">
+          <div className="arrow">
+            <img className="arrowUp" onclick="plusSlides(-1)" src="./Assets/arrowUp.svg" alt="arrowUp"/>
+            <img className="
+            arrowDown" onclick="plusSlides(1)" src="./Assets/arrowDown.svg" alt="arrowDown"/>
           </div>
         </div>
       </section>
        {/* PARTNERS  */}
-      <section id="flight" class="partners">
-        <div class="partnerLogo">
+      <section id="flight" className="partners">
+        <div className="partnerLogo">
           <a href="https://axon.com"><img src="./Assets/axon.svg" alt="axon" /></a>
         </div>
-        <div class="partnerLogo">
+        <div className="partnerLogo">
           <a href="https://jetstar.com"><img src="./Assets/jetstar.svg" alt="jetstar" /></a>
         </div>
-        <div class="partnerLogo">
+        <div className="partnerLogo">
           <a href="https://expedia.com"><img src="./Assets/expediaa.svg" alt="expedia" /></a>
         </div>
-        <div class="partnerLogo">
+        <div className="partnerLogo">
           <a href="https://qantas.com"><img src="./Assets/qantas.svg" alt="qantas" /></a>
         </div>
-        <div class="partnerLogo">
+        <div className="partnerLogo">
           <a href="https://alitalia.com"><img src="./Assets/alitalia.svg" alt="alitalia" /></a>
         </div>
       </section>
-      <section id="subscribe" class="subscribe">
+      <section id="subscribe" className="subscribe">
          <a href="https://telegram.com"><img src="./Assets/sendBig.svg" alt="telegram" /></a>
         <h3>Subscribe to get information, latest news and other
           interesting offers about Jadoo</h3>
-          <div class="ecilpse1"><img src="./Assets/eclipseRight.svg" alt="eclipseRight"></div>
-          <div class="ecilpse2"><img src="./Assets/eclipseLeft.svg" alt="eclipseLeft"></div>
+          <div className="ecilpse1"><img src="./Assets/eclipseRight.svg" alt="eclipseRight"/></div>
+          <div className="ecilpse2"><img src="./Assets/eclipseLeft.svg" alt="eclipseLeft"/></div>
           <form action="">
-            <img src="./Assets/mail.svg" alt="mail">
+            <img src="./Assets/mail.svg" alt="mail"/>
           <label for="Email">  </label>
             <input type="email" name="Email"  id="Email" placeholder="Your email" />
             <input type="submit" value="Subscribe" id="submit" name="Subscribe" />
             <span id="errorEmail"></span>
         </form>
-        <div class="littlestars">
-          <img src="./Assets/littleStarDown.svg" alt="littleStarDown">
+        <div className="littlestars">
+          <img src="./Assets/littleStarDown.svg" alt="littleStarDown"/>
         </div>
       </section>
     </main> 
       
     </div>
   )
-}\
+}
 
 export default Main
