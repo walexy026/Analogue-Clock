489b5ba8cbb820149a359cbc936bec497bf838be
olawale <jao.olaniyi@gmail.com>
2022-09-17 18:28:48 +0100

    implemented the UI design

--- src/Component/Clock/Clock.jsx
+++ src/Component/Clock/Clock.jsx
@@ -5,21 +5,26 @@ const Clock = () => {
   return (
     <div className="main">
       <div className="clock">
+        <span className="secs-hand"></span>
+        <span className="min-hand"></span>
+        <span className="hour-hand"></span>
         <span className="middle-dot"></span>
-        <span className="one">1</span>
-        <span className="two">2</span>
-        <span className="three">3</span>
-        <span className="four">4</span>
-        <span className="five">5</span>
-        <span className="six">6</span>
-        <span className="seven">7</span>
-        <span className="eight">8</span>
-        <span className="nine">9</span>
-        <span className="ten">10</span>
-        <span className="eleven">11</span>
-        <span className="twelve">12</span>
+        
+        <span className="number one">1</span>
+        <span className=" number two">2</span>
+        <span className=" number three">3</span>
+        <span className="number four">4</span>
+        <span className="number five">5</span>
+        <span className="number six">6</span>
+        <span className="number seven">7</span>
+        <span className="number eight">8</span>
+        <span className="number nine">9</span>
+        <span className="number ten">10</span>
+        <span className="number eleven">11</span>
+        <span className="number twelve">12</span>
+        </div>
       </div>
-    </div>
+
   );
 };
 
