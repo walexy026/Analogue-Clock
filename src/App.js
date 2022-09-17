fd6e6f890049772dbce4f67b724886f746556ab8
olawale <jao.olaniyi@gmail.com>
2022-09-17 15:47:07 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
-import Main from './Components/Main/Main';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
+      
     </div>
   );
 }
